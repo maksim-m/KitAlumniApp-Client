c89b9962b82e332e17561f907b8aef074f335071
Stelian Stoev <ugdhx@student.kit.edu>
2015-01-19 18:12:46 +0100

    Fragmente aktualisiert. Alle Fragmente sind Blank und das Projekt sollte jetzt bereinigt.

--- app/src/main/java/edu/kit/isco/kitalumniapp/fragments/EventSearchResultsViewFragment.java
+++ app/src/main/java/edu/kit/isco/kitalumniapp/fragments/EventSearchResultsViewFragment.java
@@ -13,12 +13,12 @@ import edu.kit.isco.kitalumniapp.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link DetailsViewFragment.OnFragmentInteractionListener} interface
+ * {@link EventSearchResultsViewFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link DetailsViewFragment#newInstance} factory method to
+ * Use the {@link EventSearchResultsViewFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class DetailsViewFragment extends Fragment {
+public class EventSearchResultsViewFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,7 +30,7 @@ public class DetailsViewFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
-    public DetailsViewFragment() {
+    public EventSearchResultsViewFragment() {
         // Required empty public constructor
     }
 
@@ -40,11 +40,11 @@ public class DetailsViewFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment DetailsViewFragment.
+     * @return A new instance of fragment SearchFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static DetailsViewFragment newInstance(String param1, String param2) {
-        DetailsViewFragment fragment = new DetailsViewFragment();
+    public static EventSearchResultsViewFragment newInstance(String param1, String param2) {
+        EventSearchResultsViewFragment fragment = new EventSearchResultsViewFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -65,7 +65,7 @@ public class DetailsViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_details_view, container, false);
+        return inflater.inflate(R.layout.fragment_event_search, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
