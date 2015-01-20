94b6f293adc5cd7785446b406fcce3758df2d577
Stelian Stoev <ugdhx@student.kit.edu>
2015-01-20 16:40:21 +0100

    Fragmentfehler entfernt. Jetzt sind auch in GCM Branch die richtigen Fragmente drin.

--- app/src/main/java/edu/kit/isco/kitalumniapp/fragments/EventDetailsViewFragment.java
+++ app/src/main/java/edu/kit/isco/kitalumniapp/fragments/EventDetailsViewFragment.java
@@ -13,12 +13,12 @@ import edu.kit.isco.kitalumniapp.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link OverViewFragment.OnFragmentInteractionListener} interface
+ * {@link EventDetailsViewFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link OverViewFragment#newInstance} factory method to
+ * Use the {@link EventDetailsViewFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class OverViewFragment extends Fragment {
+public class EventDetailsViewFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -30,26 +30,26 @@ public class OverViewFragment extends Fragment {
 
     private OnFragmentInteractionListener mListener;
 
+    public EventDetailsViewFragment() {
+        // Required empty public constructor
+    }
+
     /**
      * Use this factory method to create a new instance of
      * this fragment using the provided parameters.
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment OverViewFragment.
+     * @return A new instance of fragment DetailsViewFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static OverViewFragment newInstance(String param1, String param2) {
-        OverViewFragment fragment = new OverViewFragment();
+    public static EventDetailsViewFragment newInstance(String param1, String param2) {
+        EventDetailsViewFragment fragment = new EventDetailsViewFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-
-    public OverViewFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -65,7 +65,7 @@ public class OverViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_over_view, container, false);
+        return inflater.inflate(R.layout.fragment_event_details_view, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
--- app/src/main/java/edu/kit/isco/kitalumniapp/fragments/NewsSearchResultsViewFragment.java
+++ app/src/main/java/edu/kit/isco/kitalumniapp/fragments/NewsSearchResultsViewFragment.java
@@ -13,12 +13,12 @@ import edu.kit.isco.kitalumniapp.R;
 /**
  * A simple {@link Fragment} subclass.
  * Activities that contain this fragment must implement the
- * {@link OverViewFragment.OnFragmentInteractionListener} interface
+ * {@link NewsSearchResultsViewFragment.OnFragmentInteractionListener} interface
  * to handle interaction events.
- * Use the {@link OverViewFragment#newInstance} factory method to
+ * Use the {@link NewsSearchResultsViewFragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class OverViewFragment extends Fragment {
+public class NewsSearchResultsViewFragment extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
@@ -36,11 +36,11 @@ public class OverViewFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment OverViewFragment.
+     * @return A new instance of fragment NewsSearchResultsViewFragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static OverViewFragment newInstance(String param1, String param2) {
-        OverViewFragment fragment = new OverViewFragment();
+    public static NewsSearchResultsViewFragment newInstance(String param1, String param2) {
+        NewsSearchResultsViewFragment fragment = new NewsSearchResultsViewFragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -48,7 +48,7 @@ public class OverViewFragment extends Fragment {
         return fragment;
     }
 
-    public OverViewFragment() {
+    public NewsSearchResultsViewFragment() {
         // Required empty public constructor
     }
 
@@ -65,7 +65,7 @@ public class OverViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_over_view, container, false);
+        return inflater.inflate(R.layout.fragment_news_search_results_view, container, false);
     }
 
     // TODO: Rename method, update argument and hook method into UI event
