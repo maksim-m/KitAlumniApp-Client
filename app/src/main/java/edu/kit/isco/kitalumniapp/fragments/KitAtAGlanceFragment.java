dada9c90f62829da1e8196ab24f3d040f0bb3303
ykelle <yannickkeller@web.de>
2015-01-30 14:56:39 +0100

    Habe die PDF bzgl. Kit at a glance als Webview eingearbeitet. Dies geschieht mit Hilfe von google docs.

--- app/src/main/java/edu/kit/isco/kitalumniapp/fragments/KitAtAGlanceFragment.java
+++ app/src/main/java/edu/kit/isco/kitalumniapp/fragments/KitAtAGlanceFragment.java
@@ -6,6 +6,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.webkit.WebView;
 
 import edu.kit.isco.kitalumniapp.R;
 
@@ -21,13 +22,23 @@ public class KitAtAGlanceFragment extends Fragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_kit_at_aglance, container, false);
+        //return inflater.inflate(R.layout.fragment_kit_at_aglance, container, false);
+
+        //Create Webview, which contain a path to GoogleDocs which open
+        // the PdfUrl to the recomment document
+        String myPdfUrl = "http://www.kit.edu/mediathek/print_forschung/Broschuere_KIT_Ueberblick_en.pdf";
+        View rootView = inflater.inflate(R.layout.fragment_kit_navi, container, false);
+        WebView webView = (WebView) rootView.findViewById(R.id.webViewCampus);
+        webView.loadUrl("http://docs.google.com/gview?embedded=true&url=" + myPdfUrl);
+        webView.getSettings().setJavaScriptEnabled(true);
+        return rootView;
     }
 
     @Override
