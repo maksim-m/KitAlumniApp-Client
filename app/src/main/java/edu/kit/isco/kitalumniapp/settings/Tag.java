d3f7a992356c60d3697bfb64828fb5e3921d5981
Kristina Mincheva <kristina@kristina.com>
2015-02-09 00:53:24 +0100

    Kategorie Tags in Setting ist fast fertig, es bleibt noch den Save-Button.

---
+++ app/src/main/java/edu/kit/isco/kitalumniapp/settings/Tag.java
@@ -0,0 +1,21 @@
+package edu.kit.isco.kitalumniapp.settings;
+
+/**
+ * Created by Kristina on 7.2.2015 г..
+ */
+public class Tag {
+
+    String name = null;
+
+    public Tag(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+}
