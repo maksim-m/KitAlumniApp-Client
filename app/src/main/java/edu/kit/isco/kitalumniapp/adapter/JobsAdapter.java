4592069beee80ea77d056f53acb8ed1afad94b7b
Kristina Mincheva <kristina@kristina.com>
2015-02-11 17:53:57 +0100

    Neuer Zweig für die JobsListe

---
+++ app/src/main/java/edu/kit/isco/kitalumniapp/adapter/JobsAdapter.java
@@ -0,0 +1,13 @@
+package edu.kit.isco.kitalumniapp.adapter;
+
+import android.widget.ArrayAdapter;
+
+import edu.kit.isco.kitalumniapp.dbObjects.DataAccessJob;
+
+/**
+ * Created by Kristina on 11.2.2015 г..
+ */
+public class JobsAdapter extends ArrayAdapter<DataAccessJob> {
+
+
+}
