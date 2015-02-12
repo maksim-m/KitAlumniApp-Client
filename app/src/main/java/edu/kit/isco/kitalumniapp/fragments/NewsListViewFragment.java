fd2c8c145ebb6ad491a1d273eba242308d485b9b
ioksrom <ioksrom@gmail.com>
2015-02-12 01:44:50 +0100

    Dummy SwipeRefreshLayout.

--- app/src/main/java/edu/kit/isco/kitalumniapp/fragments/NewsListViewFragment.java
+++ app/src/main/java/edu/kit/isco/kitalumniapp/fragments/NewsListViewFragment.java
@@ -2,12 +2,13 @@ package edu.kit.isco.kitalumniapp.fragments;
 
 import android.app.Activity;
 import android.app.Fragment;
-import android.app.ListFragment;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
+import android.widget.AbsListView;
 import android.widget.ListView;
 
 import com.koushikdutta.async.future.Future;
@@ -23,13 +24,14 @@ import edu.kit.isco.kitalumniapp.dbObjects.DataAccessNews;
  */
 public class NewsListViewFragment extends Fragment {
 
-    NewsAdapter adapter;
+    private NewsAdapter adapter;
     // This "Future" tracks loading operations.
     // A Future is an object that manages the state of an operation
     // in progress that will have a "Future" result.
     // You can attach callbacks (setCallback) for when the result is ready,
     // or cancel() it if you no longer need the result.
-    Future<List<DataAccessNews>> loading;
+    private Future<List<DataAccessNews>> loading;
+    private SwipeRefreshLayout swipeRefreshLayout;
 
     public NewsListViewFragment() {
         // Required empty public constructor
@@ -44,10 +46,43 @@ public class NewsListViewFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_news_list_view, container, false);
+        View view = inflater.inflate(R.layout.fragment_news_list, container, false);
+        swipeRefreshLayout = (SwipeRefreshLayout) view.findViewById(R.id.newsSwipeContainer);
+        swipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                swipeRefreshLayout.setRefreshing(true);
+                swipeRefreshLayout.postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        swipeRefreshLayout.setRefreshing(false);
+                    }
+                }, 4500);
+            }
+        });
+        // sets the colors used in the refresh animation
+        swipeRefreshLayout.setColorSchemeResources(R.color.kit_cyan_blau, R.color.kit_darker_green);
         adapter = new NewsAdapter(getActivity(), 0);
-        ListView listView = (ListView) view.findViewById(R.id.newsListView);
+        final ListView listView = (ListView) view.findViewById(R.id.newsListView);
         listView.setAdapter(adapter);
+        listView.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView view, int scrollState) { }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                boolean enable = false;
+                if(listView != null && listView.getChildCount() > 0){
+                    // check if the first item of the list is visible
+                    boolean firstItemVisible = listView.getFirstVisiblePosition() == 0;
+                    // check if the top of the first item is visible
+                    boolean topOfFirstItemVisible = listView.getChildAt(0).getTop() == 0;
+                    // enabling or disabling the refresh layout
+                    enable = firstItemVisible && topOfFirstItemVisible;
+                }
+                swipeRefreshLayout.setEnabled(enable);
+            }
+        });
         adapter.loadLatest();
         return view;
 
