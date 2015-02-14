6eb9539897ecbf0d0cf62b599ea30be0272493bc
Stelian Stoev <ugdhx@student.kit.edu>
2015-02-14 03:47:21 +0100

    GCM Verkehr durch REST umgeleitet. Update Methode implementiert. DataAccessUser hinzugefügt.

---
+++ app/src/main/java/edu/kit/isco/kitalumniapp/dbObjects/DataAccessUser.java
@@ -0,0 +1,47 @@
+package edu.kit.isco.kitalumniapp.dbObjects;
+
+import java.util.List;
+
+/**
+ * Created by Stelian Stoev on 13.2.2015 г..
+ */
+public class DataAccessUser implements DataAccessObject {
+
+
+    private String clientId;
+    private List<DataAccessTag> tags;
+    private String password;
+
+    public DataAccessUser() {
+    }
+
+    public DataAccessUser(String clientId, List<DataAccessTag> tags, String password) {
+        this.clientId = clientId;
+        this.tags = tags;
+        this.password = password;
+    }
+
+    public String getClientId() {
+        return clientId;
+    }
+
+    public void setClientId(String clientId) {
+        this.clientId = clientId;
+    }
+
+    public List<DataAccessTag> getTags() {
+        return tags;
+    }
+
+    public void setTags(List<DataAccessTag> tags) {
+        this.tags = tags;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
--- app/src/main/java/edu/kit/isco/kitalumniapp/gcm/ServerUtilities.java
+++ app/src/main/java/edu/kit/isco/kitalumniapp/gcm/ServerUtilities.java
@@ -21,186 +21,196 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
-import java.io.IOException;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
+import com.google.gson.JsonObject;
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.List;
 import java.util.Random;
 
 import edu.kit.isco.kitalumniapp.R;
+import edu.kit.isco.kitalumniapp.dbObjects.DataAccessTag;
+import edu.kit.isco.kitalumniapp.dbObjects.DataAccessUser;
 
 /**
  * Created by Stelian Stoev on 14.1.2015 г..
  */
 public class ServerUtilities {
 
-        static final String SERVER_URL = "http://s18028446.onlinehome-server.info:8080/KitAlumniApp-Server2";
-        /**
-         * Tag used on log messages.
-         */
-        static final String TAG = "GCMDemo";
-
-        private static final int MAX_ATTEMPTS = 5;
-        private static final int BACKOFF_MILLI_SECONDS = 2000;
-        private static final Random random = new Random();
-
-        /**
-         * Register this account/device pair within the server.
-         *
-         */
-        public void register(final Context context, final String regId) {
-            Log.i(TAG, "registering device (regId = " + regId + ")");
-            String serverUrl = SERVER_URL + "/register";
-            Map<String, String> params = new HashMap<String, String>();
-            params.put("regId", regId);
-            long backoff = BACKOFF_MILLI_SECONDS + random.nextInt(1000);
-            // Once GCM returns a registration id, we need to register it in the
-            // demo server. As the server might be down, we will retry it a couple
-            // times.
-            for (int i = 1; i <= MAX_ATTEMPTS; i++) {
-                Log.d(TAG, "Attempt #" + i + " to register");
-                try {
-                    displayMessage(context, context.getString(
-                            R.string.server_registering, i, MAX_ATTEMPTS));
-                    post(serverUrl, params);
-                    //GCMRegistrar.setRegisteredOnServer(context, true);
-                    String message = context.getString(R.string.server_registered);
-                    displayMessage(context, message);
-                    return;
-                } catch (IOException e) {
-                    // Here we are simplifying and retrying on any error; in a real
-                    // application, it should retry only on unrecoverable errors
-                    // (like HTTP error code 503).
-                    Log.e(TAG, "Failed to register on attempt " + i + ":" + e);
-                    if (i == MAX_ATTEMPTS) {
-                        break;
-                    }
-                    try {
-                        Log.d(TAG, "Sleeping for " + backoff + " ms before retry");
-                        Thread.sleep(backoff);
-                    } catch (InterruptedException e1) {
-                        // Activity finished before we complete - exit.
-                        Log.d(TAG, "Thread interrupted: abort remaining retries!");
-                        Thread.currentThread().interrupt();
-                        return;
-                    }
-                    // increase backoff exponentially
-                    backoff *= 2;
-                }
-            }
-            String message = context.getString(R.string.server_register_error,
-                    MAX_ATTEMPTS);
-            displayMessage(context, message);
-        }
-
-        /**
-         * Unregister this account/device pair within the server.
-         */
-        public void unregister(final Context context, final String regId) {
-            Log.i(TAG, "unregistering device (regId = " + regId + ")");
-            String serverUrl = SERVER_URL + "/unregister";
-            Map<String, String> params = new HashMap<String, String>();
-            params.put("regId", regId);
-            try {
-                post(serverUrl, params);
-                //GCMRegistrar.setRegisteredOnServer(context, false);
-                String message = context.getString(R.string.server_unregistered);
-                displayMessage(context, message);
-            } catch (IOException e) {
-                // At this point the device is unregistered from GCM, but still
-                // registered in the server.
-                // We could try to unregister again, but it is not necessary:
-                // if the server tries to send a message to the device, it will get
-                // a "NotRegistered" error message and should unregister the device.
-                String message = context.getString(R.string.server_unregister_error,
-                        e.getMessage());
-                displayMessage(context, message);
-            }
-        }
-
-        /**
-         * Issue a POST request to the server.
-         *
-         * @param endpoint POST address.
-         * @param params request parameters.
-         *
-         * @throws IOException propagated from POST.
-         */
-        private void post(String endpoint, Map<String, String> params)
-                throws IOException {
-            URL url;
-            try {
-                url = new URL(endpoint);
-            } catch (MalformedURLException e) {
-                throw new IllegalArgumentException("invalid url: " + endpoint);
-            }
-            StringBuilder bodyBuilder = new StringBuilder();
-            Iterator<Map.Entry<String, String>> iterator = params.entrySet().iterator();
-            // constructs the POST body using the parameters
-            while (iterator.hasNext()) {
-                Map.Entry<String, String> param = iterator.next();
-                bodyBuilder.append(param.getKey()).append('=')
-                        .append(param.getValue());
-                if (iterator.hasNext()) {
-                    bodyBuilder.append('&');
-                }
-            }
-            String body = bodyBuilder.toString();
-            Log.v(TAG, "Posting '" + body + "' to " + url);
-            byte[] bytes = body.getBytes();
-            HttpURLConnection conn = null;
-            try {
-                conn = (HttpURLConnection) url.openConnection();
-                conn.setDoOutput(true);
-                conn.setUseCaches(false);
-                conn.setFixedLengthStreamingMode(bytes.length);
-                conn.setRequestMethod("POST");
-                conn.setRequestProperty("Content-Type",
-                        "application/x-www-form-urlencoded;charset=UTF-8");
-                // post the request
-                OutputStream out = conn.getOutputStream();
-                out.write(bytes);
-                out.close();
-                // handle the response
-                int status = conn.getResponseCode();
-                if (status != 200) {
-                    throw new IOException("Post failed with error code " + status);
-                }
-            } finally {
-                if (conn != null) {
-                    conn.disconnect();
-                }
-            }
-        }
-
+    static final String SERVER_URL = "http://s18028446.onlinehome-server.info:8080/KitAlumniApp-Server2/rest/service/users/";
+    /**
+     * Tag used on log messages.
+     */
+    static final String TAG = "GCMDemo";
     /**
      * Intent used to display a message in the screen.
      */
     static final String DISPLAY_MESSAGE_ACTION =
             "com.google.android.gcm.demo.app.DISPLAY_MESSAGE";
-
     /**
      * Intent's extra that contains the message to be displayed.
      */
     static final String EXTRA_MESSAGE = "message";
+    private static final int MAX_ATTEMPTS = 5;
+    private static final int BACKOFF_MILLI_SECONDS = 2000;
+    private static final Random random = new Random();
 
     /**
      * Notifies UI to display a message.
-     * <p>
+     * <p/>
      * This method is defined in the common helper because it's used both by
      * the UI and the background service.
      *
      * @param context application's context.
      * @param message message to be displayed.
      */
-     static void displayMessage(Context context, String message) {
+    static void displayMessage(Context context, String message) {
         Intent intent = new Intent(DISPLAY_MESSAGE_ACTION);
         intent.putExtra(EXTRA_MESSAGE, message);
         context.sendBroadcast(intent);
     }
 
+    /**
+     * Register this account/device pair within the server.
+     */
+    public void register(final Context context, final String regId) {
+        Log.i(TAG, "registering device (regId = " + regId + ")");
+        SecureRandom password = new SecureRandom();
+
+        DataAccessUser user = new DataAccessUser(regId, null, new BigInteger(64, password).toString(32));
+        long backoff = BACKOFF_MILLI_SECONDS + random.nextInt(1000);
+        // Once GCM returns a registration id, we need to register it in the
+        // app server. The registration will be repeated maximal 5 times.
+        for (int i = 1; i <= MAX_ATTEMPTS; i++) {
+            Log.d(TAG, "Attempt #" + i + " to register");
+            try {
+                displayMessage(context, context.getString(
+                        R.string.server_registering, i, MAX_ATTEMPTS));
+                Ion.with(context)
+                        .load("POST", SERVER_URL)
+                        .setJsonPojoBody(user)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+                                Log.i(TAG, "Registration delivered to APP Server");
+                            }
+                        });
+                String message = context.getString(R.string.server_registered);
+                displayMessage(context, message);
+                return;
+            } catch (Exception e) {
+                // Here we are simplifying and retrying on any error; in a real
+                // application, it should retry only on unrecoverable errors
+                // (like HTTP error code 503).
+                Log.e(TAG, "Failed to register on attempt " + i + ":" + e);
+                if (i == MAX_ATTEMPTS) {
+                    break;
+                }
+                try {
+                    Log.d(TAG, "Sleeping for " + backoff + " ms before retry");
+                    Thread.sleep(backoff);
+                } catch (InterruptedException e1) {
+                    // Activity finished before we complete - exit.
+                    Log.d(TAG, "Thread interrupted: abort remaining retries!");
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+                // increase backoff exponentially
+                backoff *= 2;
+            }
+        }
+        String message = context.getString(R.string.server_register_error,
+                MAX_ATTEMPTS);
+        displayMessage(context, message);
+    }
+
+    /**
+     * Unregister this account/device pair within the server.
+     */
+    public void unregister(final Context context, final String regId) {
+        Log.i(TAG, "unregistering device (regId = " + regId + ")");
+        DataAccessUser user = new DataAccessUser(regId, null, null);
+        try {
+            Ion.with(context)
+                    .load("DELETE", SERVER_URL)
+                    .setJsonPojoBody(user)
+                    .asJsonObject()
+                    .setCallback(new FutureCallback<JsonObject>() {
+                        @Override
+                        public void onCompleted(Exception e, JsonObject result) {
+                            Log.i(TAG, "Unregistrating Success");
+                        }
+                    });
+            //GCMRegistrar.setRegisteredOnServer(context, false);
+            String message = context.getString(R.string.server_unregistered);
+            displayMessage(context, message);
+        } catch (Exception e) {
+            // At this point the device is unregistered from GCM, but still
+            // registered in the server.
+            // We could try to unregister again, but it is not necessary:
+            // if the server tries to send a message to the device, it will get
+            // a "NotRegistered" error message and should unregister the device.
+            String message = context.getString(R.string.server_unregister_error,
+                    e.getMessage());
+            displayMessage(context, message);
+        }
+    }
+
+    /**
+     * Updating user
+     */
+
+    public void updateUser(Context context, List<DataAccessTag> tags, String regId) {
+        Log.i(TAG, "registering device (regId = " + regId + ")");
+
+        SecureRandom password = new SecureRandom();
+        DataAccessUser user = new DataAccessUser(regId, tags, new BigInteger(64, password).toString(32));
+        long backoff = BACKOFF_MILLI_SECONDS + random.nextInt(1000);
+        // The update will be repeated maximal 5 times.
+        for (int i = 1; i <= MAX_ATTEMPTS; i++) {
+            Log.d(TAG, "Attempt #" + i + " to update");
+            try {
+                displayMessage(context, context.getString(
+                        R.string.server_registering, i, MAX_ATTEMPTS));
+                Ion.with(context)
+                        .load("PUT", SERVER_URL)
+                        .setJsonPojoBody(user)
+                        .asJsonObject()
+                        .setCallback(new FutureCallback<JsonObject>() {
+                            @Override
+                            public void onCompleted(Exception e, JsonObject result) {
+                                Log.i(TAG, "Updating info for user to APP Server");
+                            }
+                        });
+                String message = context.getString(R.string.server_registered);
+                displayMessage(context, message);
+                return;
+            } catch (Exception e) {
+                // Here we are simplifying and retrying on any error; in a real
+                // application, it should retry only on unrecoverable errors
+                // (like HTTP error code 503).
+                Log.e(TAG, "Failed to update on attempt " + i + ":" + e);
+                if (i == MAX_ATTEMPTS) {
+                    break;
+                }
+                try {
+                    Log.d(TAG, "Sleeping for " + backoff + " ms before retry");
+                    Thread.sleep(backoff);
+                } catch (InterruptedException e1) {
+                    // Activity finished before we complete - exit.
+                    Log.d(TAG, "Thread interrupted: abort remaining retries!");
+                    Thread.currentThread().interrupt();
+                    return;
+                }
+                // increase backoff exponentially
+                backoff *= 2;
+            }
+        }
+        String message = context.getString(R.string.server_register_error,
+                MAX_ATTEMPTS);
+        displayMessage(context, message);
+    }
 }
