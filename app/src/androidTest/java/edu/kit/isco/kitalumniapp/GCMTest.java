f50e1ec50770778192180ee41c9344fe08684dc8
Stelian Stoev <ugdhx@student.kit.edu>
2015-03-20 16:41:11 +0100

    ServerUtilities 100% getestet. JaCoCo test coverage tool hinzugefügt.

--- app/src/androidTest/java/edu/kit/isco/kitalumniapp/GCMTest.java
+++ app/src/androidTest/java/edu/kit/isco/kitalumniapp/GCMTest.java
@@ -1,43 +1,76 @@
 package edu.kit.isco.kitalumniapp;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.test.AndroidTestCase;
 
 import com.google.android.gms.wearable.Asset;
 
 import junit.framework.Assert;
+
+import java.util.ArrayList;
+
+import edu.kit.isco.kitalumniapp.dbObjects.DataAccessTag;
 import edu.kit.isco.kitalumniapp.gcm.ServerUtilities;
+import edu.kit.isco.kitalumniapp.settings.SettingsActivity;
 
 /**
  * Created by Stelian Stoev on 19.3.2015 г..
  */
 public class GCMTest extends AndroidTestCase {
     ServerUtilities gcmSU;
+    SharedPreferences prefs;
 
     @Override
     public void setUp() throws Exception {
         super.setUp();
+        prefs = getContext().getSharedPreferences(SettingsActivity.class.getSimpleName(),
+                Context.MODE_PRIVATE);
         gcmSU = new ServerUtilities();
     }
 
-    public void testRegisterWithEmptyRedId() {
+    public void testRegisterWithEmptyRegId() {
         try {
             gcmSU.register(getContext(), null);
             Assert.fail("IllegalArgumentException expected");
         } catch (IllegalArgumentException expected) {
         }
     }
-    public void testUpdateWithEmptyRedId() {
+
+    public void testUpdateWithEmptyRegId() {
         try {
             gcmSU.updateUser(getContext(), null, null);
             Assert.fail("IllegalArgumentException expected");
         } catch (IllegalArgumentException expected) {
         }
     }
-    public void testUnregisterWithEmptyRedId() {
+
+    public void testUnregisterWithEmptyRegId() {
         try {
             gcmSU.unregister(getContext(), null);
-            Assert.fail("IllegalArgumentException expected");
         } catch (IllegalArgumentException expected) {
+        }
+    }
+
+    public void testRegisterUser() {
+        try {
+            gcmSU.register(getContext(), "testRegId");
+        } catch (IllegalArgumentException e) {
+        }
+    }
+
+    public void testUnregisterUser() {
+        try {
+            gcmSU.unregister(getContext(), "testRegId");
+        } catch (Exception e) {
+        }
+    }
+
+    public void testUpdateUser() {
+        try {
+            gcmSU.updateUser(getContext(), new ArrayList<DataAccessTag>(), "testRegId");
+        } catch (Exception e) {
+
         }
     }
 }
