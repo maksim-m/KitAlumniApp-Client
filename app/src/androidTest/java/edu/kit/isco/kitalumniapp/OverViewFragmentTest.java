7d3d861315f768d701d09f5a2d909f01439d0afc
andla91 <andla@online.de>
2015-03-21 17:46:25 +0100

    Test für OverViewFragment hinzugefügt

---
+++ app/src/androidTest/java/edu/kit/isco/kitalumniapp/OverViewFragmentTest.java
@@ -0,0 +1,36 @@
+package edu.kit.isco.kitalumniapp;
+
+import android.app.FragmentTransaction;
+import android.test.ActivityInstrumentationTestCase2;
+
+import edu.kit.isco.kitalumniapp.fragments.OverViewFragment;
+
+/**
+ * Created by Andre on 21.03.2015.
+ */
+public class OverViewFragmentTest extends ActivityInstrumentationTestCase2<MainActivity> {
+    private MainActivity mainActivity;
+    OverViewFragment overViewFragment;
+
+    public OverViewFragmentTest() {
+        super(MainActivity.class);
+    }
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        mainActivity = getActivity();
+        startFragment();
+    }
+
+    private void startFragment() {
+        FragmentTransaction transaction = mainActivity.getFragmentManager().beginTransaction();
+        overViewFragment = new OverViewFragment();
+        transaction.add(R.id.container, overViewFragment).commitAllowingStateLoss();
+    }
+
+    public void testPreConditions() {
+        assertNotNull(mainActivity);
+        assertNotNull(overViewFragment);
+    }
+}
